72ea5ba0d93ff0ec3ff0ff47782d69111462cc2f
BeauTaapken <beau@lioncode.nl>
2025-07-11 03:00:01 +0200

    automatic push

--- automatic-push-docker-compose.sh
+++ automatic-push-docker-compose.sh
@@ -11,3 +11,9 @@ git add .
 git commit -m "automatic push"
 git push
 popd
+
+config='/usr/bin/git --git-dir=/home/beau/.cfg/ --work-tree=/home/beau'
+
+$config add -u
+$config commit -m "automatic push"
+$config push -u origin main
