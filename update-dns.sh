2b5afd28e874a786cc56c92345df5e1845ffe264
BeauTaapken <beau@lioncode.nl>
2025-09-13 03:00:10 +0200

    automatic push

--- update-dns.sh
+++ update-dns.sh
@@ -7,19 +7,23 @@ IP_FILE=${CURDIR}/.last_ip
 CURRENT_IP=$(curl https://ipinfo.io/ip)
 LAST_IP=$(cat "$IP_FILE" 2>/dev/null || echo "")
 
+echo $TOKEN
+
 if [ "$CURRENT_IP" != "$LAST_IP" ]; then
+    curl --location --request PUT "https://mijn.host/api/v2/domains/$DOMAIN/dns" \
+    	--header 'Accept: application/json' \
+	--header 'Content-Type: application/json' \
+	--header "API-Key: $TOKEN" \
+	--data @- <<-JSON
+	{
+  	    "records": [
+    		{ "type": "A", "name": "$DOMAIN.",   "value": "$CURRENT_IP", "ttl": 900 },
+    		{ "type": "A", "name": "*.$DOMAIN.", "value": "$CURRENT_IP", "ttl": 900 }
+  	    ]
+	}
+JSON
+fi
+
+if [ $? -eq 0 ]; then
     echo "$CURRENT_IP" > "$IP_FILE"
-    # curl --location --request PATCH "https://mijn.host/api/v2/domains/$DOMAIN/dns" \
-    #	--header 'Accept: application/json' \
-#	--header 'Content-Type: application/json' \
-#	--header 'API-Key: "$API_KEY"' \
-#	--data-raw '{
-#    	    "record": {
-#        	"type": "A",
-#        	"name": "$DOMAIN",
-#        	"value": "$CURRENT_IP",
-#        	"ttl": 900
-#    	    }
-#	}'
-    curl "https://www.duckdns.org/update?domains=$DOMAIN&token=$TOKEN&ip=$IP_ADDRESSi&verbose=true"
 fi
